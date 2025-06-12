f6a9a3f3597227410a4c826b2cde54e2d0b5a143
Thomas <noaillesthomas@gmail.com>
2025-06-12 17:53:07 +0200

    Add bootstrap and Sass with composer and create Home page to try if everything works

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/index', name: 'app_index_')]
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+        ]);
+    }
+}
