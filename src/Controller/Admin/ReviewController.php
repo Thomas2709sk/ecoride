84e20dd8237e5592b22018d8514a94a098ff9871
Thomas <noaillesthomas@gmail.com>
2025-06-24 13:42:49 +0200

    feat : Admin can now confirmm or delete Bad Reviews , add modals to confirm or remove , can  contact driver or passenger with mailto: . All function on Admin/ReviewController

--- src/Controller/Admin/ReviewController.php
+++ src/Controller/Admin/ReviewController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\CarpoolsUsers;
 use App\Entity\Reviews;
+use App\Entity\Users;
 use App\Repository\ReviewsRepository;
 use App\Security\Voter\ReviewsVoter;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,10 +22,10 @@ class ReviewController extends AbstractController
     {
         // check if User have 'ROLE_ADMIN'
         if (!$security->isGranted('ROLE_ADMIN')) {
-            throw $this->createAccessDeniedException('Seul un administrateur peut valider cet avis.');
+            throw $this->createAccessDeniedException('Seul un administrateur peut accéder aux avis.');
         }
 
-         // get All the reviews
+        // get All the reviews
         $reviews = $reviewsRepository->findAll();
 
         return $this->render('admin/review/index.html.twig', [
@@ -31,20 +33,27 @@ class ReviewController extends AbstractController
         ]);
     }
 
-     // Confirm good review
+    // Confirm good review
     #[Route('/confirm/{id}', name: 'confirm', methods: ['POST'])]
-    public function confirmGoodReview(Reviews $reviews, EntityManagerInterface $em, AuthorizationCheckerInterface $authChecker): Response
+    public function confirmGoodReview($id, ReviewsRepository $reviewsRepository, EntityManagerInterface $em, AuthorizationCheckerInterface $authChecker): Response
     {
-        // check if User have 'ROLE_ADMIN'
-        if (!$authChecker->isGranted(ReviewsVoter::VALID, $reviews)) {
+        $review = $reviewsRepository->find($id);
+
+        if (!$review) {
+            $this->addFlash('error', 'Avis introuvable.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+        // check if User have 'ROLE_ADMIN' or 'ROLE_STAFF'
+        if (!$authChecker->isGranted(ReviewsVoter::VALID, $review)) {
             throw $this->createAccessDeniedException('Vous n\'avez pas le droit de valider cet avis.');
         }
 
         // set Validate to true
-        $reviews->setValidate(true);
+        $review->setValidate(true);
 
 
-        $em->persist($reviews);
+        $em->persist($review);
         $em->flush();
 
         $this->addFlash('success', 'L\'avis a été validé et est maintenant visible par le chauffeur.');
@@ -52,13 +61,10 @@ class ReviewController extends AbstractController
         return $this->redirectToRoute('app_admin_review_index');
     }
 
-    // Delete review
+    // Delete review > 3 stars
     #[Route('/remove/{id}', name: 'remove')]
-    public function removeReviews(int $id, ReviewsRepository $reviewsRepository, EntityManagerInterface $em): Response
+    public function removeReviews(int $id, ReviewsRepository $reviewsRepository, EntityManagerInterface $em,  AuthorizationCheckerInterface $authChecker): Response
     {
-
-        // check if User have 'ROLE_ADMIN' or 'ROLE_STAFF'
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         // get the review to delete by its ID
         $review = $reviewsRepository->find($id);
@@ -66,7 +72,12 @@ class ReviewController extends AbstractController
         // if review don't exist
         if (!$review) {
             $this->addFlash('error', 'Avis introuvable.');
-            return $this->redirectToRoute('app_admin_reviews_index');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+        // check if User have 'ROLE_ADMIN' or 'ROLE_STAFF'
+        if (!$authChecker->isGranted(ReviewsVoter::DELETE, $review)) {
+            throw $this->createAccessDeniedException('Vous n\'avez pas le droit de supprimer cet avis.');
         }
 
         // delete review
@@ -76,5 +87,166 @@ class ReviewController extends AbstractController
         $this->addFlash('success', 'L\'avis a été supprimer avec succès.');
 
         return $this->redirectToRoute('app_admin_reviews_index');
+    }
+
+    // Confirm review < 3 stars
+    #[Route('/confirm/bad/{id}', name: 'bad', methods: ['POST'])]
+    public function confirmBadReview(
+        Reviews $review,
+        EntityManagerInterface $em,
+        AuthorizationCheckerInterface $authChecker
+    ): Response {
+
+        $carpool = $review->getCarpool();
+        if (!$carpool) {
+            $this->addFlash('error', 'Aucun covoiturage associé à cet avis.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+
+        if (!$authChecker->isGranted(ReviewsVoter::VALID, $review)) {
+            throw $this->createAccessDeniedException('Vous n\'avez pas le droit de valider cet avis.');
+        }
+
+        $user = $review->getUser();
+
+        $carpoolUser = $em->getRepository(CarpoolsUsers::class)->findOneBy([
+            'user' => $user,
+            'carpool' => $carpool,
+        ]);
+        if (!$carpoolUser) {
+            $this->addFlash('error', 'Impossible de trouver le covoiturage lier au passager.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+
+        $review->setValidate(true);
+        $carpoolUser->setIsConfirmed(true);
+
+        if ($carpool->getStatus() !== 'Vérification par la plateforme') {
+            $this->addFlash('error', 'Cet avis ne peut pas être confirmé.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+        $price = $carpool->getPrice();
+        $platformPrice = 2;
+        $driverCredits = $price - $platformPrice;
+
+        $driver = $carpool->getDriver();
+        $carpoolDriver = $driver->getUser();
+
+
+        $admin = $em->getRepository(Users::class)->find(1);
+        if (!$admin || !in_array('ROLE_ADMIN', $admin->getRoles(), true)) {
+            throw new \Exception('L\'utilisateur avec l\'ID 1 n\'est pas un administrateur.');
+        }
+
+
+        if ($carpoolUser->isConfirmed()) {
+            $carpoolDriver->setCredits($carpoolDriver->getCredits() + $driverCredits);
+            $admin->setCredits($admin->getCredits() + $platformPrice);
+        }
+
+
+        $allPassengerConfirmed = true;
+        foreach ($carpool->getCarpoolsUsers() as $cu) {
+            if (!$cu->isConfirmed()) {
+                $allPassengerConfirmed = false;
+                break;
+            }
+        }
+        if ($allPassengerConfirmed) {
+            $carpool->setStatus('Confirmé');
+        }
+
+        $em->persist($review);
+        $em->persist($carpoolUser);
+        $em->persist($carpoolDriver);
+        $em->persist($admin);
+        $em->persist($carpool);
+        $em->flush();
+
+        $this->addFlash('success', 'L\'avis négatif a été validé, est visible par le chauffeur et les crédits transférés.');
+        return $this->redirectToRoute('app_admin_review_index');
+    }
+
+    //  Delete reviews < 3 stars but still send credits to driver and admin
+    #[Route('/remove/bad/{id}', name: 'remove_bad', methods: ['POST'])]
+    public function removeBadReview(
+        Reviews $review,
+        EntityManagerInterface $em,
+        AuthorizationCheckerInterface $authChecker
+    ): Response {
+
+        $carpool = $review->getCarpool();
+        if (!$carpool) {
+            $this->addFlash('error', 'Aucun covoiturage associé à cet avis.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+
+        if (!$authChecker->isGranted(ReviewsVoter::DELETE, $review)) {
+            throw $this->createAccessDeniedException('Vous n\'avez pas le droit de supprimer cet avis.');
+        }
+
+
+        $user = $review->getUser();
+
+
+        $carpoolUser = $em->getRepository(CarpoolsUsers::class)->findOneBy([
+            'user' => $user,
+            'carpool' => $carpool,
+        ]);
+        if (!$carpoolUser) {
+            $this->addFlash('error', 'Impossible de trouver la réservation de ce passager pour ce covoiturage.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+        $carpoolUser->setIsConfirmed(true);
+
+        if ($carpool->getStatus() !== 'Vérification par la plateforme') {
+            $this->addFlash('error', 'Ce trajet ne peut pas être confirmé.');
+            return $this->redirectToRoute('app_admin_review_index');
+        }
+
+        $price = $carpool->getPrice();
+        $platformPrice = 2;
+        $driverCredits = $price - $platformPrice;
+
+        $driver = $carpool->getDriver();
+        $carpoolDriver = $driver->getUser();
+
+        $admin = $em->getRepository(Users::class)->find(1);
+        if (!$admin || !in_array('ROLE_ADMIN', $admin->getRoles(), true)) {
+            throw new \Exception('L\'utilisateur avec l\'ID 1 n\'est pas un administrateur.');
+        }
+
+        if ($carpoolUser->isConfirmed()) {
+            $carpoolDriver->setCredits($carpoolDriver->getCredits() + $driverCredits);
+            $admin->setCredits($admin->getCredits() + $platformPrice);
+        }
+
+        $allPassengerConfirmed = true;
+        foreach ($carpool->getCarpoolsUsers() as $cu) {
+            if (!$cu->isConfirmed()) {
+                $allPassengerConfirmed = false;
+                break;
+            }
+        }
+        if ($allPassengerConfirmed) {
+            $carpool->setStatus('Confirmé');
+        }
+
+        $em->remove($review);
+
+        $em->persist($carpoolUser);
+        $em->persist($carpoolDriver);
+        $em->persist($admin);
+        $em->persist($carpool);
+        $em->flush();
+
+        $this->addFlash('success', 'L\'avis a été supprimé, mais les crédits ont bien été transférés.');
+
+        return $this->redirectToRoute('app_admin_review_index');
     }
 }
